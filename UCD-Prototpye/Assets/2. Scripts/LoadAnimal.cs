3e55821521ec0c374aff67e5bed21e3cd0fa7f06
Heather Kim <haesoo1108@gmail.com>
2019-05-22 23:30:22 +0900

    implemented basic UI functions like timer and touch gauge

--- UCD-Prototpye/Assets/2. Scripts/LoadAnimal.cs
+++ UCD-Prototpye/Assets/2. Scripts/LoadAnimal.cs
@@ -3,6 +3,7 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
     using GoogleARCore;
 
 #if UNITY_EDITOR
@@ -13,29 +14,54 @@
     public class LoadAnimal : MonoBehaviour
     {
         public Camera arCamera;
-        public GameObject test;
+        public GameObject loaded;
+
+        public Text timeText;
+        public int timeLimit = 15;
+        private float timer = 0.0f;
+        private int timeInSecs = 0;
+
+        public int touchToUnlock = 20;
+        private int touchCount = 0;
+        public Slider touchSlider;
+
 
         // Start is called before the first frame update
         void Start()
         {
             arCamera = Camera.main;
+            touchSlider.maxValue = touchToUnlock;
         }
 
         // Update is called once per frame
         void Update()
         {
+            updateTimer();
+
             Touch touch = Input.GetTouch(0);
             if (Input.touchCount > 0 && touch.phase == TouchPhase.Began)
             {
-                TrackableHit hit;
-                TrackableHitFlags flags = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+                // TrackableHit hit;
+                // TrackableHitFlags flags = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
 
-                //ARCore Raycast
-                if (Frame.Raycast(touch.position.x, touch.position.y, flags, out hit))
-                {
-                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-                    Instantiate(test, hit.Pose.position, hit.Pose.rotation, anchor.transform);
-                }
+                // //ARCore Raycast
+                // if (Frame.Raycast(touch.position.x, touch.position.y, flags, out hit))
+                // {
+                //     var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                //     Instantiate(loaded, hit.Pose.position, hit.Pose.rotation, anchor.transform);
+                // }
+
+                touchCount++;
+                touchSlider.value = touchCount;
+            }
+        }
+
+        void updateTimer(){
+            timer += Time.deltaTime;
+         // turn seconds in float to int
+            timeInSecs = (int)(timer % 60);
+            if (timeInSecs<15){
+                timeText.text = "" + (timeLimit - timeInSecs);
             }
         }
     }

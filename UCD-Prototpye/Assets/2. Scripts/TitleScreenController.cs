4a6d203bc8bdeec7e533eb5e04f25de94117e552
Heather Kim <haesoo1108@gmail.com>
2019-06-03 01:00:33 +0900

    updated playroom view mode, created found screen

--- UCD-Prototpye/Assets/2. Scripts/TitleScreenController.cs
+++ UCD-Prototpye/Assets/2. Scripts/TitleScreenController.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreenController : MonoBehaviour
 {
+    
+    void Start(){
+        if(SceneManager.GetActiveScene().name.Equals("Discover") || SceneManager.GetActiveScene().name.Equals("Found")){
+            Screen.SetResolution(1088, 2224, true);
+            Screen.orientation = ScreenOrientation.Portrait;
+        } else{
+            Screen.SetResolution(2224, 1088, true);
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+    }
+
     public void toPlayRoom(){
         SceneManager.LoadScene("Playroom");
     }
---
+++ UCD-Prototpye/Assets/2. Scripts/confirmAdopt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class confirmAdopt : MonoBehaviour
+{
+    public Canvas confirmUI;
+
+    public void okay(){
+        confirmUI.enabled = true;
+    }
+}
